1c0d74b212b155bacd5183ae6e7d71d05c74c619
github username <github email address>
2017-05-17 18:04:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+
+console.log(couponLocations.reduce(couponCounter, 0)); // also prints 15!
